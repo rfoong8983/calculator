9b2beb12ead49561858989f7ae952b50c616ded2
ryan <ryanfoong8983@gmail.com>
2019-06-24 20:38:39 -0700

    Updates

---
+++ precache-manifest.f7d96101f3deab8bd48275b67baecfe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feed6ce2197c405c910cbf54cacadf71",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ed9dec038e537e29ce09",
+    "url": "/calculator/static/css/main.d6e211b8.chunk.css"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/calculator/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "ed9dec038e537e29ce09",
+    "url": "/calculator/static/js/main.9bacce8e.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "ad5512d9242060f01cf98c0047092050",
+    "url": "/calculator/static/media/logo.ad5512d9.png"
+  }
+]);
